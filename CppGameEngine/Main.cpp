f2294fe62f70591c8ef52fe2f60fe97599b95094
Mike Smullin <mike@smullindesign.com>
2017-10-04 22:02:59 -0600

    second example working first try. woo!

--- CppGameEngine/Main.cpp
+++ CppGameEngine/Main.cpp
@@ -7,10 +7,14 @@ int main(int argc, char* args[])
 	constexpr int SCREEN_WIDTH = 640;
 	constexpr int SCREEN_HEIGHT = 480;
 
+	// Initialization flag
+	bool success = true;
+
 	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		fprintf(stderr, "SDL_Init Fail: %s\n", SDL_GetError());
+		success = false;
 	}
 	else
 	{
@@ -27,18 +31,27 @@ int main(int argc, char* args[])
 			// Get window surface
 			const SDL_Surface * screenSurface = SDL_GetWindowSurface(const_cast<SDL_Window *>(window));
 
-			// Fill the surface white
+			// Fill the surface with color
 			SDL_FillRect(const_cast<SDL_Surface *>(screenSurface), NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0xFF));
+
+			const SDL_Surface * helloWorld = SDL_LoadBMP("assets/hello_world.bmp");
+			if (helloWorld == NULL) {
+				fprintf(stderr, "Failed to load image.");
+			}
+			else {
+				SDL_BlitSurface(const_cast<SDL_Surface *>(helloWorld), NULL, const_cast<SDL_Surface *>(screenSurface), NULL);
+			}
 
 			// Update the surface
 			SDL_UpdateWindowSurface(const_cast<SDL_Window *>(window));
 
 			// Wait two seconds
 			SDL_Delay(16000);
+
+			// Destroy window
+			SDL_DestroyWindow(const_cast<SDL_Window *>(window));
 		}
 
-		// Destroy window
-		SDL_DestroyWindow(const_cast<SDL_Window *>(window));
 	}
 
 	// Quit SDL subsystems
