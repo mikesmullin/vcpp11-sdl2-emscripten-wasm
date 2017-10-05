a52c1d6f06cb0fe5d38e7c008eb5407a1bf3e2bc
Mike Smullin <mike@smullindesign.com>
2017-10-04 22:03:58 -0600

    even more type strict

--- CppGameEngine/Main.cpp
+++ CppGameEngine/Main.cpp
@@ -7,19 +7,15 @@ int main(int argc, char* args[])
 	constexpr int SCREEN_WIDTH = 640;
 	constexpr int SCREEN_HEIGHT = 480;
 
-	// Initialization flag
-	bool success = true;
-
 	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		fprintf(stderr, "SDL_Init Fail: %s\n", SDL_GetError());
-		success = false;
 	}
 	else
 	{
 		// Create window
-		const SDL_Window * window = SDL_CreateWindow("Demo",
+		const SDL_Window * const window = SDL_CreateWindow("Demo",
 			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 			SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
@@ -29,12 +25,12 @@ int main(int argc, char* args[])
 		else
 		{
 			// Get window surface
-			const SDL_Surface * screenSurface = SDL_GetWindowSurface(const_cast<SDL_Window *>(window));
+			const SDL_Surface * const screenSurface = SDL_GetWindowSurface(const_cast<SDL_Window *>(window));
 
 			// Fill the surface with color
 			SDL_FillRect(const_cast<SDL_Surface *>(screenSurface), NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0xFF));
 
-			const SDL_Surface * helloWorld = SDL_LoadBMP("assets/hello_world.bmp");
+			const SDL_Surface * const helloWorld = SDL_LoadBMP("assets/hello_world.bmp");
 			if (helloWorld == NULL) {
 				fprintf(stderr, "Failed to load image.");
 			}
