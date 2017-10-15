94be84500c83ce6841a5d3cedc1ade3ae39d4426
Mike Smullin <mike@smullindesign.com>
2017-10-15 17:53:30 -0600

    got .wasm.map source map from binary to cpp working with breakpoints in firefox quantum developer edition

--- build.bat
+++ build.bat
@@ -6,10 +6,12 @@ emcc ^
 	-std=c++1z ^
 	-I include ^
 	-O2 ^
+	-g4 ^
 	-s USE_SDL=2 ^
 	-s USE_SDL_IMAGE=2 ^
 	-s SDL2_IMAGE_FORMATS="[""png""]" ^
-	--preload-file assets ^
 	-s WASM=1 ^
+	--source-map-base http://localhost:8080/ ^
 	-o build/html5/release.html
 
+REM	--preload-file assets ^
---
+++ build/html5/build/html5/src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "global.h"
+
+int main(int argc, char** argv)
+{
+	tests::GLES2Example1::setup();
+
+	std::cout << "bye" << std::endl;
+}
